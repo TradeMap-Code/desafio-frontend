41a9460d6dd11ce1bf677baa8032c172019f5aba
Ricardo Bonin <ricardo@brittes.com.br>
2021-03-10 22:55:26 -0300

    feat: Imprimir nome dos itens no card

--- trade-map/src/pages/home/index.js
+++ trade-map/src/pages/home/index.js
@@ -1,6 +1,20 @@
 import React from "react";
+import stocks from "../../stocks.json";
 
 const Home = () => {
-  return <h1>helow</h1>;
+  return (
+    <div className='container'>
+      <div className='card'>
+        {stocks &&
+          stocks.map((stock, id) => {
+            return (
+              <div key={id}>
+                <h1>{stock.company}</h1>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 export default Home;
