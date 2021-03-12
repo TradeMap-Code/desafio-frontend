876f5b2bcc761a8420c530e131a6fd8d177c90ab
Arthur Gusmao <arthur.cabj@hotmail.com>
2021-03-11 23:19:51 -0300

    Each stock card now displays it's own chart variation.

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -12,7 +12,7 @@ export function FavoriteButton({ onButtonClick, company }) {
           onButtonClick(company);
         }}
       >
-        Favoritar <AiOutlineStar />
+        <AiOutlineStar /> Favoritar
       </button>
     </div>
   );
--- src/components/StockBlock.jsx
+++ src/components/StockBlock.jsx
@@ -1,5 +1,6 @@
 import styles from '../styles/components/StockBlock.module.css';
 import { FavoriteButton } from './FavoriteButton';
+import { XYPlot, AreaSeries } from 'react-vis';
 
 export function StockBlock({
   stockName,
@@ -7,14 +8,34 @@ export function StockBlock({
   variation,
   onButtonClick,
   company,
+  chart,
 }) {
+  let xChartValue = 1;
+  const dataArr = chart.map((variation) => {
+    console.log(company)
+    return { x: xChartValue++, y: variation };
+   
+  });
+  console.log(dataArr);
+
   return (
     <div className={styles.stockWrapper}>
       <div className={styles.stockHeader}>
         <h1>{stockName}</h1>
         <FavoriteButton onButtonClick={onButtonClick} company={company} />
       </div>
-      <div className={styles.stockChart}></div>
+      <XYPlot
+        width={300}
+        height={120}
+        margin={{ left: 0, right: 0, top: 10, bottom: 10 }}
+      >
+        <AreaSeries
+          className='area-series-example'
+          curve='curveNatural'
+          data={dataArr}
+        />
+      </XYPlot>
+
       <div className={styles.stockFooter}>
         <ul>
           <li>Variação: {variation}%</li>
