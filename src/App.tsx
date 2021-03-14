385af3c3003de85701d9fa6b7d7ccff2e327aa11
Lucas Rezende <arrobagemeioponto@gmail.com>
2021-03-14 09:56:24 -0300

    feat(favorites): add (empty) Favorites tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 //---------------------------------------------------------------< components >
 import Header from "./components/Header";
-import Lista from "./components/Lista";
+import List from "./components/List";
+import Favorites from "./components/Favorites";
+//--------------------------------------------------------------------< hooks >
+import { useState } from "react";
 //-------------------------------------------------------------------< styles >
 import "./styles/global.css";
 //==================================================================[ < App > ]
 function App() {
+  //-------------------------------------------------------------< properties >
+  const [tab, setTab] = useState<"list" | "favorites">("list");
+  //----------------------------------------------------------------< methods >
   //-----------------------------------------------------------------< return >
   return (
     <div className="App">
-      <Header />
-      <Lista />
+      <Header changeTab={setTab} />
+      {tab === "list" && <List />}
+      {tab === "favorites" && <Favorites />}
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,21 @@
 import logo from "../../assets/logo.png";
 //-------------------------------------------------------------------< styles >
 import "./styles.css";
+//--------------------------------------------------------------------< types >
+interface HeaderProps {
+  changeTab: React.Dispatch<React.SetStateAction<"list" | "favorites">>;
+}
 //===============================================================[ < Header > ]
-export default function Header() {
+export default function Header({ changeTab }: HeaderProps) {
+  //----------------------------------------------------------------< methods >
+  // temporario
+  function onLogoClick() {
+    changeTab("favorites");
+  }
   //-----------------------------------------------------------------< return >
   return (
     <header className="header-container">
-      <img src={logo} alt="TradeMap" />
+      <img src={logo} alt="TradeMap" onClick={onLogoClick} />
     </header>
   );
 }
