3259947134e65874f19924365fce4d7a5e168055
Arthur Gusmao <arthur.cabj@hotmail.com>
2021-03-14 23:00:13 -0300

    Style: orthography correction.

--- src/components/DuplicateAlert.jsx
+++ src/components/DuplicateAlert.jsx
@@ -6,7 +6,7 @@ export function DuplicatedAlert({ setIsNotDuplicated, isNotDuplicated }) {
     <div className={styles.overlay} style={{ display: displayBox }}>
       <div className={styles.alertWrapper}>
         <h1>ALERTA</h1>
-        <p>Esta ação já foi adicionar a lista de favotios.</p>
+        <p>Esta ação já foi adicionada a lista de favotios.</p>
         <button type='button' onClick={() => setIsNotDuplicated(true)}>
           Entendi
         </button>
