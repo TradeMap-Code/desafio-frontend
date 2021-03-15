292f93d3fe1ee1fbee1521224f4382d3d7607a5c
Arthur Gusmao <arthur.cabj@hotmail.com>
2021-03-14 22:57:44 -0300

    Feat: new pop up apears on screen when user favorite same stock twice.

---
+++ src/components/DuplicateAlert.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/DuplicateAlert.module.css';
+
+export function DuplicatedAlert({ setIsNotDuplicated, isNotDuplicated }) {
+  const displayBox = isNotDuplicated && 'none';
+  return (
+    <div className={styles.overlay} style={{ display: displayBox }}>
+      <div className={styles.alertWrapper}>
+        <h1>ALERTA</h1>
+        <p>Esta ação já foi adicionar a lista de favotios.</p>
+        <button type='button' onClick={() => setIsNotDuplicated(true)}>
+          Entendi
+        </button>
+      </div>
+    </div>
+  );
+}
