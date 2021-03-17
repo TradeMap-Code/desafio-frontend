8d60a173ca8182a3b6168eb987d04aa7ddf11a0b
Luiz Eduardo <dudumultiplo@hotmail.com>
2021-03-17 19:28:13 -0300

    feat: delete component Chart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,22 @@
-// import React from "react";
-// import FavoriteSelect from "../components/FavoriteSelect";
-// import FinanceChart from "../components/FinanceChart";
+import React from "react";
+import FavoriteSelect from "../components/FavoriteSelect";
 
-// export default function Card({ stock }) {
-//   return (
-//     <section className="Card">
-//       <div>
-//         <h2>{stock.company}</h2>
-//         <p className="CardSubtext">
-//           <strong>Valor: $</strong>
-//           {stock.price}
-//         </p>
-//         <p className="CardSubtext">
-//           <strong>Variação: </strong>
-//           {stock.variation}
-//         </p>
-//         <p className="CardSubtext">{stock.chart}</p>
-//         <FavoriteSelect />
-//         <FinanceChart />
-//       </div>
-//     </section>
-//   );
-// }
+export default function Card({ stock }) {
+  return (
+    <section className="Card">
+      <div>
+        <h2>{stock.company}</h2>
+        <p className="CardSubtext">
+          <strong>Valor: $</strong>
+          {stock.price}
+        </p>
+        <p className="CardSubtext">
+          <strong>Variação: </strong>
+          {stock.variation}
+        </p>
+        <p className="CardSubtext">{stock.chart}</p>
+        <FavoriteSelect />
+      </div>
+    </section>
+  );
+}
